6d3096aa81ba2f2aa6a9b4a59817e01bed5f0912
Shariq Hirani <shariqhirani2010@gmail.com>
2019-05-06 16:00:50 -0500

    scaffolded

---
+++ src/main/java/com/shariq/superheromicroservices/superherofe/SuperheroFeApplication.java
@@ -0,0 +1,13 @@
+package com.shariq.superheromicroservices.superherofe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperheroFeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperheroFeApplication.class, args);
+	}
+
+}
